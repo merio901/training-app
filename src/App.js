8eabe0f81a0ab5cf8a0919c71d602b7e81aa5f5f
merio901 <micsob901@wp.pl>
2018-12-09 18:49:57 +0100

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
   render() {
     return (
       <div className="App">
