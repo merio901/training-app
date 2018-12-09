490ff41a01e6c6c502900783988138b0cada2045
merio901 <micsob901@wp.pl>
2018-12-09 19:28:07 +0100

    edit App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => console.log(json))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(json => console.log(json));
   }
   render() {
     return (
       <div className="App">
-        <div>
-          Hello World!
-        </div>
+        <div>Hello World!!!</div>
       </div>
     );
   }
