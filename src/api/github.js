0e6b9955f2b8a1ea65a09f0fc2392b97f2722cea
Michał Sobczak <micsob901@wp.pl>
2020-07-13 20:13:17 +0200

    Promises

--- src/api/github.js
+++ src/api/github.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_VERSION = "https://api.github.com/";
+export const API_VERSION = "https://api.github.com/";
 
-export const getUser = user => axios.get(`${API_VERSION}users/${user}`);
+export const getUser = (user) => axios.get(`${API_VERSION}users/${user}`);
 
 export const getEmojis = () => axios.get(`${API_VERSION}emojis`);
--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,21 @@
 import React, { Component } from "react";
-import { getUser, getEmojis } from "../api/github";
-import { searchPhotos } from "../api/unsplash";
 
-const renderLine = (user, key) => (
-  <li key={key}>
-    <b>{key}</b>: {user[key]}
-  </li>
-);
-
-const renderImage = element => (
-  <img key={element.id} alt="XD" src={element.urls.small} />
-);
 class App extends Component {
-  state = {
-    user: {},
-    images: [],
-    emojis: {},
-  };
-
   componentDidMount() {
-    getUser('merio901').then(data => {
-      this.setState({ user: data.data });
-    });
-    getEmojis().then(data => {
-      this.setState({ emojis: data.data });
-    });
-    searchPhotos("kitty").then(data => {
-      this.setState({
-        images: data,
-      });
-    });
+    fetch("https://api.punkapi.com/v2/beers?per_page=5").then(
+      // onFullfilment handler - gets executed when promise is resolved
+      (res) => {
+        return Promise.reject("error").then((data) => console.log(data));
+      },
+      // onRejected handler - gets executed when promise is rejected
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 
-  fetchPhotos = e => {
-    e.preventDefault();
-    searchPhotos("balls").then(data => {
-      this.setState({
-        images: data,
-      });
-    });
-  };
-
   render() {
-    const { user, emojis, images } = this.state;
-    return (
-      <div>
-        <ul style={{ listStyle: "none" }}>
-          {// Loop over the object keys and render each key
-          Object.keys(user).map(key => renderLine(user, key))}
-        </ul>
-        <ul style={{ listStyle: "none" }}>
-          {// Loop over the object keys and render each key
-          Object.keys(emojis).map(key => renderLine(emojis, key))}
-        </ul>
-        <form>
-          <input type="text" />
-          <button onClick={e => this.fetchPhotos(e)}>Get photos!</button>
-        </form>
-        <ul style={{ listStyle: "none" }}>{images.map(e => renderImage(e))}</ul>
-      </div>
-    );
+    return <div>hello</div>;
   }
 }
 
